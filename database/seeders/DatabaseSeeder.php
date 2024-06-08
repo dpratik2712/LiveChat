e80d12d3cd944d579130ad9b912f4bb4fcf6ce82
Dubey Pratik <pratikdubey9724@gmail.com>
2024-06-08 22:04:15 +0530

    No changes has been commited. just a little bit update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,16 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@user.com',
+        ]);
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+        ]);
+        User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'test@test.com',
         ]);
     }
 }
