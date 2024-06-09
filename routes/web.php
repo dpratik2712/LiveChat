e66e09c06fde5c572e9785f1fff7656b99463f3c
Dubey Pratik <pratikdubey9724@gmail.com>
2024-06-09 12:08:48 +0530

    added a chat interface in view chat-component

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('/dashboard', function () {
+    $users = User::where('id', '!=', auth()->user()->id)->get();
+    return view('dashboard', [
+        'users' => $users
+    ]);
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::get('/chat/{id}', function ($id) {
+    return view('chat', [
+        'id' => $id
+    ]);
+})->middleware(['auth', 'verified'])->name('chat');
+
+
+
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
